6c4a578f3628df03272c27f21e5d506a3b292813
Raphael van Akkerveken <raphaelvakkerveken@hotmail.com>
2022-08-02 00:13:01 +0200

    Added framework to start working with

---
+++ src/StatRoller/API/Hubs/StatRollerHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace DndPocketAssistant.StatRoller.API.Hubs;
+
+public class StatRollerHub : Hub
+{
+    public async Task RollStats(string user)
+    {
+        // TODO: Implement rolls.. should be depending on selected roll type by me with local console tool
+        List<int> rollResults = null;
+        await Clients.All.SendAsync("ReceiveRolls", user, rollResults);
+    }
+}
---
+++ src/StatRoller/Tests/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndPocketAssistant.StatRoller.Tests
+{
+    internal class Class1
+    {
+    }
+}
